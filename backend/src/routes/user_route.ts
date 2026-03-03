591808b39ce85f93148b2caee134f9154869705d
Ilay Shefer <ilay90411@gmail.com>
2026-03-03 17:42:43 +0200

    feat(user): add user service

--- backend/src/routes/user_route.ts
+++ backend/src/routes/user_route.ts
@@ -1,10 +1,12 @@
 import express from 'express'
-import Message from '../controllers/message'
+import UserController from '../controllers/user'
 import authMiddleware from '../common/auth_middleware'
 
 const router = express.Router()
+
 router.use(authMiddleware)
 
-router.get('/:contactId', Message.getChatHistory)
+router.get('/:id', UserController.getUserById)
+router.patch('/', UserController.updateUser)
 
 export default router
