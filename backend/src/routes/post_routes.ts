da4d79b0cd7352e0a7759c197380e3bbd8b48c5b
Ilay Shefer <ilay90411@gmail.com>
2026-04-22 23:22:43 +0300

    add comment tests

--- backend/src/routes/post_routes.ts
+++ backend/src/routes/post_routes.ts
@@ -472,10 +472,10 @@ const router = express.Router()
 
 router.post('/', authMiddleware, PostController.createPost.bind(PostController))
 router.post('/aiSearch', PostController.freeSearchPosts.bind(PostController))
-router.get('/',authMiddleware, PostController.getPosts.bind(PostController))
-router.get('/:authorId',authMiddleware, PostController.getPosts.bind(PostController))
+router.get('/', authMiddleware, PostController.getPosts.bind(PostController))
+router.get('/:authorId', authMiddleware, PostController.getPosts.bind(PostController))
 router.post('/comment/:id', authMiddleware, PostController.addComment.bind(PostController))
-router.put('/like/:id',authMiddleware,PostController.ToggleLike.bind(PostController))
+router.put('/like/:id', authMiddleware, PostController.toggleLike.bind(PostController))
 router.put('/:id', authMiddleware, PostController.updatePost.bind(PostController))
 
 /**
